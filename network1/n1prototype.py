8f4d63cd4a069c000809ca547c35e4cddaee1216
Will Kearns <kearns391@gmail.com>
2015-08-26 14:07:22 -0400

    'fixes issue with the format of resArray'

--- network1/n1prototype.py
+++ network1/n1prototype.py
@@ -7,7 +7,7 @@ Created on Wed Aug 12 09:49:47 2015
 This is a prototype for a neural network that will identify tomato images.
 It requires two directories, one consisting exclusively of .png tomato images
 the other consisting exclusively of .png non-tomato images
-the idea is to read both of these in and append them togeth using resizetoInputArray
+the idea is to read both of these in and append them together using resizetoInputArray
 then create a corresponding output array based on the length of these, with (0,1) indicating a positive result
 and (1,0) indicating a negative result. (We could switch this, doesn't really matter lol)
 
@@ -35,22 +35,25 @@ import glob
 
 script_dir = os.path.dirname(__file__)
 positivepath = "data/tomatoGeneral/*.png" # due to glob mechanics, *.png must be added, filters to only png images
-positivepath = "data/nonTomatoGeneral/*.jpg" 
+negativepath = "data/nonTomatoGeneral/*.jpg"
 
 
-def resizetoInputArray(resArray,path): #takes in list to store it, as well as a path
-    abs_file_path = os.path.join(script_dir, path) #add subdirectory to our global path
-    files=glob.glob(abs_file_path) #use Glob to get a whole list of the images
+def resizetoInputArray(resArray,path): # takes in list to store it, as well as a path
+    abs_file_path = os.path.join(script_dir, path) # add subdirectory to our global path
+    files=glob.glob(abs_file_path) # use Glob to get a whole list of the images
     for item in files: 
-        im = misc.imread(item) #read it in
+        im = misc.imread(item) # read it in
         print(len(im))
-        imResize = misc.imresize(im,(50,50)) #resize it to 50x50
+        imResize = misc.imresize(im,(50,50))   # resize it to 50x50
         print(len(imResize))
-        imFlattened = imResize.flatten() #flatten that shit        
-        print(len(imFlattened)) #should be 7500, a lot are 10000? #also, need to get rid of that weird encoding tuple stuff
-        #that tuple encoding stuff is causing a vectorspace error in the densedesignmatrix that tomatoidentifier stems from
+        imFlattened = imResize.flatten()       # flatten that shit
+        print(len(imFlattened))                # should be 7500, a lot are 10000?
+        # also, need to get rid of that weird encoding tuple stuff
+        # that tuple encoding stuff is causing a vectorspace error in the densedesignmatrix
+        # that tomatoidentifier stems from
         resArray.append(imFlattened)
-    return resArray	
+    resized = np.vstack([array[:7500] for array in resArray])   # only [numbersweneed] remain
+    return resized
 
 
 class TomatoIdentifier(DenseDesignMatrix):
@@ -58,15 +61,15 @@ class TomatoIdentifier(DenseDesignMatrix):
         X = []
         Y = []
         self.class_names = ['0', '1']
-        X = resizetoInputArray(X,positivepath) 
+        X = resizetoInputArray(X, positivepath)
         temp = len(X)
         i = 0
         while i < temp:
-            Y.append((1,0))
-            i = i + 1
-        #X += resizetoInputArray(X,negativepath) 
-        #temp2 = len(X)-temp
-        #for i in temp2:
+            Y.append((1, 0))
+            i += 1
+        # X += resizetoInputArray(X,negativepath)
+        # temp2 = len(X)-temp
+        # for i in temp2:
         #    Y.append((0,1))#need to fill the outputs, repeat (1,0) for size of positivepath, () for size of negative
         X = np.array(X)
         Y = np.array(Y)
@@ -92,7 +95,7 @@ while True:
     ann.monitor()
     if not trainer.continue_learning(ann):
         break
-
+'''
 inputs = np.array([[0, 0]])
 print ann.fprop(theano.shared(inputs, name='inputs')).eval()
 inputs = np.array([[0, 1]])
@@ -101,3 +104,4 @@ inputs = np.array([[1, 0]])
 print ann.fprop(theano.shared(inputs, name='inputs')).eval()
 inputs = np.array([[1, 1]])
 print ann.fprop(theano.shared(inputs, name='inputs')).eval()
+'''
