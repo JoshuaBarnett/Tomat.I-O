18fddd5604edd4a635d4ee3f000a3a96ece29eb2
JoshuaBarnett <joshua.d.barnett@gmail.com>
2015-09-08 20:15:01 -0400

    Added rudamentary file logging for network1, several updates to tomato_cropper(only outputs grayscale though)

--- tomato_cropper/tomatoCropper.py
+++ tomato_cropper/tomatoCropper.py
@@ -14,6 +14,9 @@ resizedImages = resized to 50 x 50
 
 The functions named crop don't actually crop, just find circles,  I never got
 the circles to reliably work so I didn't figure out a way to crop stuff out
+
+a complementary program that might help to develop would be one to filter through a directory, show images and have user assign as tomato or not, then sort accordingly.
+With heuristically mined data, it would allow us to maintain 100% accuracy with greatly increased throughput time
 '''
 
 def resize_images(image_list):
@@ -36,45 +39,47 @@ def get_images():
 
 #from a CV tutorial about finding circles
 def testCV(image_list):
-	os.chdir('./tomatoImages')
-	cropped_images = {}
+    os.chdir('./tomatoImages')
+    cropped_images = {}
 
-	for image in image_list:
-		img = cv2.imread(image,0)
-		img = cv2.medianBlur(img,5)
-		cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-
-		'''
-		May want to experiment with different min/max radii, if the min is too
+    for image in image_list:
+        img = cv2.imread(image,0) # read in image
+        img2 = cv2.medianBlur(img,5) #duplicate and blur
+        img2 = cv2.Canny(img2,.9,.6,3) #canny edge detect on blurred
+        cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR) #not sure
+        circles = cv2.HoughCircles(img2,cv.CV_HOUGH_GRADIENT,1,20,
+		                            param1=100,param2=50,minRadius=10,maxRadius=75) #find circcles in detected edge img (play with params to improve performance)
+        circles = np.uint16(np.around(circles)) #make nparray of [[xcent,ycent,radius]]
+        '''
+         May want to experiment with different min/max radii, if the min is too
 		small, it will find circles in everything, also high resolution images
-		take forever, may want to scale them down first
-		'''
-		circles = cv2.HoughCircles(img,cv.CV_HOUGH_GRADIENT,1,20,
-		                            param1=50,param2=30,minRadius=0,maxRadius=0)
+		take forever, may want to scale them down first param1 and 2 seem to have sig. effect on results
+        '''
+    cropped_images['cropped_' + image] = cimg #?
+   ''' #maybe split function here?'''
+    os.chdir('../croppedImages') #switch to cropped directory
+    croppedImgPadSize = 30 #HOW MUCH PIXEL PADDING AROUND CIRCLE WHEN CROPPED
+    for i in circles[0,:]:#for each circle found
+        print str(i[0]) + " " + str(i[1]) + " " + str(i[2]) #print circle info
+        cv2.imwrite(str(xx)+".png",img[i[0]-i[2]-croppedImgPadSize:i[2]+i[0]+croppedImgPadSize,i[1]-i[2]-croppedImgPadSize:i[1]+i[2]+croppedImgPadSize]) #write img of circle contents w/ pad
+        if i[2] > 100:#?
+# draw the outer circle
+            cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+# draw the center of the circle
+            cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-		circles = np.uint16(np.around(circles))
-		for i in circles[0,:]:
-			if i[2] > 100:
-			    # draw the outer circle
-			    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-			    # draw the center of the circle
-			    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-		cropped_images['cropped_' + image] = cimg
+    #for image in cropped_images:
+    #    cv2.imwrite(image, cimg)
 
-	os.chdir('../croppedImages')
-
-	for image in cropped_images:
-		cv2.imwrite(image, cimg)
-
-	return cropped_images.keys()
+    return cropped_images.keys() #?
 
 def crop_and_resize(image_list):
 	cropped_images = testCV(image_list)
 	resize_images(cropped_images) #current dir = croppedImages 
-
+'''#maybe resize before saving file? will savve hella memory+time'''
 
 def main():
-	crop_and_resize(get_images())
+	testCV(get_images())
 
 main()
